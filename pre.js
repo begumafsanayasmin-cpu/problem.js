c8d532e821a7340ab28b82a0f3ce45ad251fb6d0
Afsana <begumafsanayasmin2009@gmali,com>
2026-09-16 23:03:33 +0530

    Add new file

---
+++ pre.js
@@ -0,0 +1,30 @@
+full_name = "afsana";
+age = 17;
+Price = 120;
+city = "Dergaon";
+
+
+console.log("full_name")
+console.log("full_name: " + full_name);
+console.log(full_name)
+
+console.log("age")
+console.log("age: "+age);
+console.log(age);
+
+
+console.log("Price: "+Price);
+console.log("city: "+city);
+
+function getResult(marks, passMark=40){
+    if(mark >=80){
+        return "A"
+    }else if(mark >=60){
+        return "B"
+    }else if (mark >= passMark){
+        return "c"
+    }else{
+        
+    }
+        
+}
