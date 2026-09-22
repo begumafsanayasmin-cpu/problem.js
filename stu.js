f6b3af840873af84927ddc848a05a6359b53dcfe
Afsana <begumafsanayasmin2009@gmali,com>
2026-09-22 16:34:33 +0530

    new file

---
+++ stu.js
@@ -0,0 +1,16 @@
+let studentName="saniya";
+let age=18;
+let course="B.tech";
+let collegename="SITM";
+let marks=70;
+let birthyear=2007;
+
+marks=75;
+age=20;
+
+console.log("StudentName",studentName);
+console.log("Age",age);
+console.log("Course",course);
+console.log("Collegename",collegename);
+console.log("Marks",marks);
+console.log("Birthyear",birthyear);
